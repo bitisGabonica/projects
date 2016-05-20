777c4cf32ffee00e561410e92522bac2ca582799
Elyott Clepper <Elyott Clepper>
2016-05-20 17:34:09 -0400

    updated again

--- plantFinder3000/plantScript.js
+++ plantFinder3000/plantScript.js
@@ -1,21 +1,44 @@
 
 $('#Go').click(function(){
 
-  console.log('hello?');
 var hardinessVal =$('#climateZone').val();
 var soilDrainage = $('#soilDrainage').val();
+var plantboxCheck = $('.plantbox').val();
 console.log(hardinessVal);
 console.log(soilDrainage);
 
 
 $('.plantdiv').each(function(){
 if ( ($(this).hasClass(hardinessVal) &&
-($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain'))){
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised'))) &&
+//added this in to try to make the loop cycle through the checkboxes as well
+($("#droughtTolerant").is(':checked'))
+{
   $(this).show();
-console.log($(this));
+
 }
-else {
-  console.log('else')
+//so if it doesnt return anything for 'droughtTolerant' it should run again and see if it finds a checked box for the other variables
+//if it doesn't find any checked boxes at all, it should just run a vanilla function without any checkbox search methods
+else if ( ($(this).hasClass(hardinessVal) &&
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised')))&&
+($("#heatTolerant").is(':checked'))
+{
+  $(this).show();
+
 }
+
+else if ( ($(this).hasClass(hardinessVal) &&
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised')))&&
+($("#coldTolerant").is(':checked'))
+{
+  $(this).show();
+
+}
+else ( ($(this).hasClass(hardinessVal) &&
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised')))
+{
+  $(this).show();
+
+};
 });
 });
