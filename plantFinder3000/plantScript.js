46e0cf2f67134561589521aff16b9b68b3dfdfec
Anthony Bouvier <thebouv@gmail.com>
2016-05-26 10:01:43 -0400

    fixed matching parentheses errors on 3 conditionals

--- plantFinder3000/plantScript.js
+++ plantFinder3000/plantScript.js
@@ -10,9 +10,9 @@ console.log(soilDrainage);
 
 $('.plantdiv').each(function(){
 if ( ($(this).hasClass(hardinessVal) &&
-($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised'))) &&
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised')) &&
 //added this in to try to make the loop cycle through the checkboxes as well
-($("#droughtTolerant").is(':checked'))
+($("#droughtTolerant").is(':checked')))
 {
   $(this).show();
 
@@ -20,16 +20,16 @@ if ( ($(this).hasClass(hardinessVal) &&
 //so if it doesnt return anything for 'droughtTolerant' it should run again and see if it finds a checked box for the other variables
 //if it doesn't find any checked boxes at all, it should just run a vanilla function without any checkbox search methods
 else if ( ($(this).hasClass(hardinessVal) &&
-($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised')))&&
-($("#heatTolerant").is(':checked'))
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised'))&&
+($("#heatTolerant").is(':checked')))
 {
   $(this).show();
 
 }
 
 else if ( ($(this).hasClass(hardinessVal) &&
-($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised')))&&
-($("#coldTolerant").is(':checked'))
+($(this).hasClass(soilDrainage)|| soilDrainage === 'Uncertain/Raised'))&&
+($("#coldTolerant").is(':checked')))
 {
   $(this).show();
 
